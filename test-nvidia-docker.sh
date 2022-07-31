1883b28254b043958d7492345be65a95d161aaf3
Behzad Samadi <behzad.samadi@gmail.com>
2022-07-31 08:48:07 -0400

    test nvidia docker

--- test-nvidia-docker.sh
+++ test-nvidia-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
-docker run -it --rm --gpus all ubuntu nvidia-smi
+# docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi
+docker run --rm --gpus all ubuntu nvidia-smi
